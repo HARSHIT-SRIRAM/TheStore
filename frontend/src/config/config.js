df2a146be460377fac7d2d0c515fdbaf14ff043f
Harshit <sriramharshit4859@gmail.com>
2024-08-19 18:47:13 +0530

    vercel

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  apiUrl: "the-store-k9dbom3qm-harshit-srirams-projects.vercel.app",
 };
 
 export default config;
